904909bb963d482e546e0f0228b2c632ddbf6091
bagwanbulbul <bulbulbagwan918@gmail.com>
2019-06-04 00:44:10 +0530

    added invalid input

--- my_game.py
+++ my_game.py
@@ -2,3 +2,6 @@ import random
 my_list = ["rock", "paper", "scissor"]
 computer =random.choice(my_list)
 user_input = raw_input("enter the rock, sesior, paper")
+
+if user_input not in my_list:
+	print "invalid input"
