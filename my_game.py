d363c8dec79f09d81d679b97084cabd108aff7b5
bagwanbulbul <bulbulbagwan918@gmail.com>
2019-06-06 10:11:40 +0530

    Rock Paper Scissor Game

--- my_game.py
+++ my_game.py
@@ -1,7 +1,32 @@
 import random
 my_list = ["rock", "paper", "scissor"]
 computer =random.choice(my_list)
-user_input = raw_input("enter the rock, sesior, paper")
 
-if user_input not in my_list:
-	print "invalid input"
+while True:
+	user_input = raw_input("enter the rock, scisor, paper ")
+    
+	if user_input not in my_list:
+		print "invalid input"
+	elif user_input == computer:
+		print "Draw"
+	elif user_input == "paper" and computer == "rock":
+		print "win"
+	elif user_input == "rock" and computer == "scissor":
+		print "win"
+	elif user_input == "scissor" and computer == "paper":
+		print "win"
+	else:
+		print "loss"
+		
+	again_input=raw_input("Do you want to play again(yes/no)")
+
+	if again_input == "yes":
+		continue
+	else:
+		break
+
+
+
+
+
+	
